a24f311d39e8a75e67deb19cd86dfaca571b032d
👻👽🤖🧙‍♂️🫠 <sprain6628@gmail.com>
2025-02-05 18:35:08 +0900

    feat: Implement Initial UI

---
+++ src/main/java/com/sprain6628/background_adder/Controller.java
@@ -0,0 +1,17 @@
+package com.sprain6628.background_adder;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.SplitPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+import java.util.Objects;
+
+public class Controller {
+
+
+    public void initialize() {
+
+    }
+}
